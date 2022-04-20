7ec495481dbc9c7cc18f49f8c80c70f5307fad5d
Joe Walters <info@thenakeddeveloper.com>
2022-04-20 23:08:30 +0100

    Start of reveal of internals from api  to test project

---
+++ Tests/ComponentTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class ComponentTests
+    {
+        [Fact]
+        public async Task Some_Random_Test()
+        {
+            var webAppFactory = new WebApplicationFactory<WebAPI.Program>();
+            var httpClient = webAppFactory.CreateDefaultClient();
+
+            var response = await httpClient.GetAsync("");
+            var result = await response.Content.ReadAsStringAsync();
+
+            result.Should().Contain("");
+        }
+    }
+}
