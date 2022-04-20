b054e9bc37b535cbd759ba11d5cfecebae201aaf
Joe Walters <info@thenakeddeveloper.com>
2022-04-21 00:12:57 +0100

    Fix internals refernce and use inline within code reference instead of editing the project file (left for reference)

--- Source/WebAPI/Program.cs
+++ Source/WebAPI/Program.cs
@@ -13,8 +13,8 @@ namespace WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
