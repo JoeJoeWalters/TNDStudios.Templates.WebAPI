ed2acf8d8f530490514cb4a569f867befd1902ff
TNDStudios <info@thenakeddeveloper.com>
2022-05-25 00:31:18 +0100

    Address casting to AddressVM tests

--- Source/Domain/AddressVM.cs
+++ Source/Domain/AddressVM.cs
@@ -23,5 +23,18 @@ namespace Domain
         public CountryVM Country { get; set; } = new CountryVM() { };
 
         public string PostalCode { get; set; } = string.Empty;
+
+        public AddressVM(Address source)
+        {
+            this.Id = source.Id;
+            this.Addressee = source.Addressee;
+            this.AddressLine1 = source.AddressLine1;
+            this.AddressLine2 = source.AddressLine2;
+            this.AddressLine3 = source.AddressLine3;
+            this.AddressLine4 = source.AddressLine4;
+            this.CountyState = source.CountyState;
+            this.PostalCode = source.PostalCode;
+            this.Country = new CountryVM(source.CountryCode);
+        }
     }
 }
--- Tests/AddressTests.cs
+++ Tests/AddressTests.cs
@@ -17,8 +17,47 @@ namespace Tests
         }
 
         [Theory]
+        [InlineData()]
+        public void When_Address_Cast_To_AddressVM() 
+        {
+            // Arrange
+            string expected2DigitCode = "GB";
+            string expectedCountryName = "United Kingdom";
+            Address address = 
+                new Address() 
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Addressee = "Addressee",
+                    AddressLine1 = "AddressLine1",
+                    AddressLine2 = "AddressLine2",
+                    AddressLine3 = "AddressLine3",
+                    AddressLine4 = "AddressLine4",
+                    CountryCode = "GBR",
+                    CountyState = "CountyState",
+                    PostalCode = "PS12BBB"
+                };
+
+            // Act
+            AddressVM result = new AddressVM(address);
+
+            // Assert
+            result.Id.Should().Be(address.Id);
+            result.Addressee.Should().Be(address.Addressee);
+            result.AddressLine1.Should().Be(address.AddressLine1);
+            result.AddressLine2.Should().Be(address.AddressLine2);
+            result.AddressLine3.Should().Be(address.AddressLine3);
+            result.AddressLine4.Should().Be(address.AddressLine4);
+            result.Country.Should().NotBeNull();
+            result.Country.CountryCode.Should().Be(address.CountryCode);
+            result.Country.CountryCode2.Should().Be(expected2DigitCode);
+            result.Country.CountryName.Should().Be(expectedCountryName);
+            result.CountyState.Should().Be(address.CountyState);
+            result.PostalCode.Should().Be(address.PostalCode);
+        }
+
+        [Theory]
         [InlineData("GBR", "GB", "United Kingdom")]
-        public void When_3Digit_Address_Used_Resolves_Object_Properties(string countryCode, string expected2Digit, string expectedName)
+        public void When_3Digit_Address_Exists(string countryCode, string expected2Digit, string expectedName)
         {
             // Arrange
             CountryVM resolvedObject;
