f2712dd7ddaf95e40695f15d707b2f0a28aa9637
TNDStudios <info@thenakeddeveloper.com>
2022-05-24 20:46:22 +0100

    Address Test Baseline

---
+++ Tests/AddressTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using Domain;
+
+namespace Tests
+{
+    public class AddressTests
+    {
+        public AddressTests()
+        {
+
+        }
+
+        [Theory]
+        [InlineData("GBR", "GB", "United Kingdom")]
+        public void When_3Digit_Address_Used_Resolves_Object_Properties(string countryCode, string expected2Digit, string expectedName)
+        {
+            // Arrange
+            CountryVM resolvedObject;
+
+            // Act
+            resolvedObject = new CountryVM(countryCode);
+
+            // Assert
+            resolvedObject.Should().NotBeNull();
+            resolvedObject.CountryCode2.Should().Be(expected2Digit);
+            resolvedObject.CountryName.Should().Be(expectedName);
+        }
+    }
+}
