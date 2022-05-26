3fffe82b8752481972aac982db8283d377ef9ad5
TNDStudios <info@thenakeddeveloper.com>
2022-05-26 01:05:29 +0100

    Including generated XML Comments as part of swagger generation

--- Source/WebAPI/Program.cs
+++ Source/WebAPI/Program.cs
@@ -1,6 +1,8 @@
 using System.Runtime.CompilerServices;
 using System;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 
 [assembly: InternalsVisibleTo("Tests")]
 
@@ -37,7 +39,30 @@ namespace WebAPI
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(
+                options =>
+                {
+                    options.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "ToDo API",
+                        Description = "An ASP.NET Core Web API for managing ToDo items",
+                        TermsOfService = new Uri("https://example.com/terms"),
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Example Contact",
+                            Url = new Uri("https://example.com/contact")
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Example License",
+                            Url = new Uri("https://example.com/license")
+                        }
+                    });
+
+                    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+                });
 
             var app = builder.Build();
 
