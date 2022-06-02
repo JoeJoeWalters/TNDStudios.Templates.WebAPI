6a45074f957e20b9a66615260414754fab6eb111
TNDStudios <info@thenakeddeveloper.com>
2022-06-02 15:21:18 +0100

    Added in loading of coverage report generator and compilation of results.

--- Tests/TestRunner.bat
+++ Tests/TestRunner.bat
@@ -1,6 +1,10 @@
+dotnet tool install -g dotnet-reportgenerator-globaltool
+pause
 FOR /D /r %%G in ("*.Tests") DO (
 	Echo We found %%~nxG
 	cd %%~nxG && dotnet test --collect:"XPlat Code Coverage" --filter "InMemory=yes"
 	cd ..
 )
+pause
+reportgenerator "-reports:./**/coverage.cobertura.xml" "-targetdir:./coverage" "-reporttypes:Html"
 pause
