4bc59a8e91a0df4b32f1da412c08e54a7b9ed816
Joe Walters <info@thenakeddeveloper.com>
2022-06-06 18:39:39 +0100

    Updates for checking if running

--- Source/WebAPI/Workers/Worker.cs
+++ Source/WebAPI/Workers/Worker.cs
@@ -7,6 +7,16 @@ namespace WebAPI
     {
         private readonly ILogger<Worker> _logger;
 
+        private Boolean _isRunning = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Boolean IsRunning 
+        { 
+            get { return _isRunning; } 
+            set { _isRunning = value; } 
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -23,10 +33,23 @@ namespace WebAPI
         /// <returns></returns>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            // To ensure good recovery then don't let exceptions other than cancellation exceptions bubble out of here
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Running operation at '{time}'", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                try
+                {
+                    _isRunning = true;
+                    _logger.LogInformation("Running operation at '{time}'", DateTimeOffset.Now);
+                    await Task.Delay(1000, stoppingToken);
+                }
+                catch (OperationCanceledException ex)
+                {
+                    return; // Exit the infinite loop
+                }
+                catch (Exception ex)
+                {
+                    // Handle any errors and ensure the while loop isn't broken so we can recover
+                }
             }
         }
     }
